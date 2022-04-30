9a0bee5bc3532552ac5cb4687a320de8aa78db37
TuanIT <hoangdinhtuana37@gmail.com>
2022-04-30 11:18:26 +0700

    báo cáo cuoi ky

---
+++ sourcecode/test_search.py
@@ -0,0 +1,27 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    def test_search(self):
+        driver = self.driver
+        driver.get("https://www.techwithtim.net")
+        self.assertIn('techwithtim', driver.title)
+        elem = driver.find_element_by_name("s")
+        elem.send_keys("python")
+        elem.send_keys(Keys.RETURN)
+        time.sleep(10)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
